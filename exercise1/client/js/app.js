b5168477e6bd8a42da6a84d74ba1bc0a84422c1c
Dan Johnson <dan@balancerockmedia.com>
2014-10-25 11:44:51 -0400

    edits

--- exercise1/client/js/app.js
+++ exercise1/client/js/app.js
@@ -4,14 +4,14 @@ angular.module('Demo', []);
 // main controller
 angular.module('Demo').controller('MainCtrl', function($scope) {
     'use strict';
-    
+
     $scope.name = 'Dan';
 });
 
 // local user controller
 angular.module('Demo').controller('LocalUserCtrl', function($scope) {
     'use strict';
-    
+
     $scope.localUsers = [{
         firstName: 'Dan',
         lastName: 'Johnson'
@@ -22,7 +22,7 @@ angular.module('Demo').controller('LocalUserCtrl', function($scope) {
         firstName: 'Ava',
         lastName: 'Johnson'
     }];
-    
+
     $scope.addUser = function() {
         $scope.localUsers.push({
             firstName: 'Jorden',
@@ -36,7 +36,7 @@ angular.module('Demo').constant('ServerUrl', 'http://localhost:3000/');
 // remote user controller
 angular.module('Demo').controller('RemoteUserCtrl', function($scope, $http, ServerUrl) {
     'use strict';
-    
+
     $http.get(ServerUrl + 'users').success(function(response) {
         $scope.remoteUsers = response;
     });
@@ -44,19 +44,18 @@ angular.module('Demo').controller('RemoteUserCtrl', function($scope, $http, Serv
     $scope.createUser = function(user) {
         $http.post(ServerUrl + 'users', user).success(function(response) {
             $scope.remoteUsers.push(response);
-            
-            $scope.user.firstName = '';
-            $scope.user.lastName = '';
+
+            $scope.user = {};
         });
     };
-    
+
     $scope.deleteUser = function(user) {
         $http.delete(ServerUrl + 'users/' + user.id).success(function(response) {
             // remove from users array by id
             for (var i = 0; i < $scope.remoteUsers.length; i++){
                 if ($scope.remoteUsers[i].id == user.id) {
                     $scope.remoteUsers.splice(i, 1);
-                    
+
                     break;
                 }
             }
