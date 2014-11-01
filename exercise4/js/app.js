dd4d1c19615840e5f84b5cba4a7b8e1af816f7d4
Dan Johnson <dan@balancerockmedia.com>
2014-11-01 12:13:55 -0400

    edits

--- exercise4/js/app.js
+++ exercise4/js/app.js
@@ -4,16 +4,33 @@ angular.module('Exercise4', [
     'Exercise4.directives',
 ]);
 
-angular.module('Exercise4.directives').directive('demoDirective', function() {
+angular.module('Exercise4.directives').directive('demoLorem', function() {
     return {
         restrict: 'E', // element
-        
-        template: '<p>Hello World!</p>',
 
-        link: function($scope, element, attrs) {
-            element.on('click', function() {
+        template: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+    };
+});
+
+angular.module('Exercise4.directives').directive('demoClickable', function() {
+    return {
+        restrict: 'A', // attribute
+
+        link: function($scope, element) {
+            // element is wrapped at jQuery object
+            element.bind('click', function() {
                 alert('Hello');
             });
         }
+    };
+});
+
+angular.module('Exercise4.directives').directive('demoFeedback', function() {
+    return {
+        restrict: 'E',
+        scope: {
+            placeholder: '@' // one way, top down
+        },
+        templateUrl: 'templates/feedback.html'
     };
 });
