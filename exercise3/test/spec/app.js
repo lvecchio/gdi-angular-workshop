1ecd094f089f7f8a48baf424c4c3803ae3fbf89c
Dan Johnson <dan@balancerockmedia.com>
2014-11-01 10:28:08 -0400

    edits

---
+++ exercise3/test/spec/app.js
@@ -0,0 +1,37 @@
+describe('Controller: Exercise3', function() {
+    'use strict';
+
+    var NavbarCtrl,
+        scope,
+        $location;
+
+    // mock the controller's module
+    beforeEach(angular.mock.module('Exercise3'));
+
+    // initialize the controller and a mock scope
+    beforeEach(angular.mock.inject(function($controller, $rootScope, _$location_) {
+        scope = $rootScope.$new();
+
+        $location = _$location_;
+
+        NavbarCtrl = $controller('NavbarCtrl', {
+            $scope: scope
+        });
+    }));
+
+    it('isActive() should return true if when passed the current location', function() {
+        $location.path('/about');
+
+        expect($location.path()).toBe('/about');
+
+        expect(scope.isActive('/about')).toBe(true);
+    });
+
+    it('isActive() should return false if when passed a different location', function() {
+        $location.path('/about');
+
+        expect($location.path()).toBe('/about');
+
+        expect(scope.isActive('/contact')).toBe(false);
+    });
+});
